3f6958b5e5d201d8c7ecef02a157e100bf2d7805
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-29 00:03:29 +0200

    added ToDoService to the providers array.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TodoService} from '../todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
+  todoListArray: any[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private todoService: TodoService) {
   }
 
+  ngOnInit() {
+    this.todoService.getTodoList().snapshotChanges().subscribe(
+      item => {
+        this.todoListArray = [];
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          x['$key'] = element.key;
+          console.log(x);
+          this.todoListArray.push(x);
+        });
+        this.todoListArray.sort((a, b) => {
+            return a.isChecked - b.isChecked;
+          }
+        );
+      }
+    );
+  }
+
+  addItem(title: HTMLInputElement) {
+    this.todoService.setTitle(title.value);
+    title.value = null;
+  }
+
+  updateCheck($key: string, isChecked: boolean) {
+    this.todoService.checkOrUncheckTask($key, !isChecked);
+  }
+
+  deleteTask($key: string) {
+    this.todoService.removeTask($key);
+  }
 }
