d06978e564f59697113638b600a0eaa60886e4f9
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-28 21:27:34 +0200

    added configuration for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAUqoocwzMddI69Ar_u0hobBWom8htVpcY',
+    authDomain: 'todoapp-11d1b.firebaseapp.com',
+    databaseURL: 'https://todoapp-11d1b.firebaseio.com',
+    projectId: 'todoapp-11d1b',
+    storageBucket: 'todoapp-11d1b.appspot.com',
+    messagingSenderId: '275178686707'
+  }
 };
