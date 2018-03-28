04977a2643c40dc14b0ac864a8d872f7f0055a87
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-28 22:27:53 +0200

    implemented todoService methods.

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 
 @Injectable()
 export class TodoService {
+  todoList: AngularFireList<any>;
 
-  constructor() { }
+  constructor(private firebasedb: AngularFireDatabase) {
+  }
 
+  getTodoList() {
+    this.todoList = this.firebasedb.list('titles');
+    return this.todoList;
+  }
+
+  setTitle(title: string) {
+    this.todoList.push(
+      {
+        title: title,
+        isChecked: false
+      }
+    );
+  }
+
+  checkOrUncheckTask($key: string, flag: boolean) {
+    this.todoList.update($key, flag);
+  }
+
+  removeTask($key: string) {
+    this.todoList.remove($key);
+  }
 }
